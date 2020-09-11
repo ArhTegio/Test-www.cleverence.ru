ea2870e504fd470fe6e181757682c077d20600bd
Arhio <Arhio@yandex.ru>
2020-09-11 12:08:48 +0300

    Выполнение задания на C#

---
+++ Test-www.cleverence.ru/Test1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace Test_www.cleverence.ru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Parallel.For(1, 10, x =>
+            {
+                Server.AddToCount(x);
+                var list = new List<int>();
+
+                for (int i = 0; i < 10; ++i)
+                {
+                    list.Add(Server.GetCount().Result);
+                }
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
